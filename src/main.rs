8fb9756a887eaa871225da884e804b45c200a116
Joshua Hill <josh@pyxxilated.studio>
2020-12-30 00:19:59 +1300

    Remove Firmware, Modify Marquee Display

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 #![no_std]
 #![no_main]
+#![feature(slice_fill)]
 
 use panic_halt as _;
 
 use embedded_graphics::fonts::{Font6x8, Text};
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::*;
-use embedded_graphics::primitives::Rectangle;
-use embedded_graphics::{primitive_style, text_style};
+use embedded_graphics::text_style;
 use gd32vf103xx_hal::delay::McycleDelay;
 use gd32vf103xx_hal::pac;
 use gd32vf103xx_hal::prelude::*;
@@ -32,20 +32,15 @@ fn main() -> ! {
 
     let lcd_pins = lcd_pins!(gpioa, gpiob);
     let mut lcd = lcd::configure(dp.SPI0, lcd_pins, &mut afio, &mut rcu);
-    let (width, height) = (lcd.size().width as i32, lcd.size().height as i32);
+    let (width, _) = (lcd.size().width as i32, lcd.size().height as i32);
 
     let style = text_style!(
         font = Font6x8,
-        text_color = Rgb565::BLACK,
-        background_color = Rgb565::GREEN
+        text_color = Rgb565::GREEN,
+        background_color = Rgb565::BLACK
     );
 
-    //Text::new(" Hi Simon! ", Point::new(40, 35))
-    //    .into_styled(style)
-    //    .draw(&mut lcd)
-    //    .unwrap();
-
-    let text = " Hi Simon! ";
+    let text = "Hi!";
 
     let mut delay = McycleDelay::new(&rcu.clocks);
 
@@ -54,15 +49,18 @@ fn main() -> ! {
     loop {
         lcd.clear(Rgb565::BLACK).unwrap();
 
-        text.chars().enumerate().for_each(|(idx, _)| {
-            Text::new(&text[idx..idx + 1], Point::new(i + (idx * 6) as i32, 35))
+        (0..text.len())
+            .map(|idx| {
+                Text::new(
+                    &text[idx..idx + 1],
+                    Point::new((i + (idx * 6) as i32) % width, 35),
+                )
                 .into_styled(style)
-                .draw(&mut lcd)
-                .unwrap();
-        });
+            })
+            .for_each(|ch| ch.draw(&mut lcd).unwrap());
 
-        i = (i + 1) % width;
+        i = if i - 6 < 0 { width } else { i - 6 };
 
-        delay.delay_ms(33);
+        delay.delay_ms(50);
     }
 }
